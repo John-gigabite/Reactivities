90a43a2c2e85107bd421ee62856a9fb74edf2397
John Gomez <gomezjohn389@gmail.com>
2023-09-07 19:31:02 -0500

    End of section 2

---
+++ Domain/Activity.cs
@@ -0,0 +1,17 @@
+namespace Domain
+{
+    //This file is an Entity/Model
+    //This is what we use to store info in DB
+    public class Activity
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public string City { get; set; }
+        public string Venue { get; set; }
+        
+
+    }
+}
