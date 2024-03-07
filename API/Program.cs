1de3d75a022fbc677d9d46f1ecfe64096e9323ad
John Gomez <gomezjohn389@gmail.com>
2024-03-07 14:38:55 -0600

    Add inital files

---
+++ API/Program.cs
@@ -0,0 +1,50 @@
+
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+//Getting access to a Service
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+try
+{
+    //Getting a hold of the DataContext Service
+    var context = services.GetRequiredService<DataContext>();
+    //Doing the same as the database Update command in the CLI
+    await context.Database.MigrateAsync();
+    await Seed.SeedData(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    //Displays exception
+    logger.LogError(ex, "An error occured during mirgation");
+}
+
+app.Run();
